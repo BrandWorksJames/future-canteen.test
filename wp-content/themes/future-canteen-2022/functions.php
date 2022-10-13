09b496e8221b93ecd756e67a4bcb70d1966f2753
James Phillips <james@brandworks.co>
2022-10-13 12:19:27 +1100

    Added Favicon

--- wp-content/themes/future-canteen-2022/functions.php
+++ wp-content/themes/future-canteen-2022/functions.php
@@ -176,3 +176,11 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+/**
+ * Load Favicon.
+ */
+
+function add_favicon(){
+    echo "<link rel='shortcut icon' href='" . get_stylesheet_directory_uri() . "/favicon.png' />" . "\n";
+}
+add_action( 'wp_head', 'add_favicon');
