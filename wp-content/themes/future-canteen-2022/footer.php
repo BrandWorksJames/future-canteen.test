7cbd549645806f20bd6761653a81ef43d59705d8
James Phillips <james@brandworks.co>
2022-10-13 18:16:17 +1100

    Added Footer

--- wp-content/themes/future-canteen-2022/footer.php
+++ wp-content/themes/future-canteen-2022/footer.php
@@ -1,31 +1,40 @@
 <?php
 /**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package Future_Canteen_2022
- */
+* The template for displaying the footer
+*
+* Contains the closing of the #content div and all content after.
+*
+* @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+*
+* @package Future_Canteen_2022
+*/
 
 ?>
 
-	<footer id="colophon" class="site-footer">
+<footer id="colophon" class="site-footer">
+	<div id="footer-inner">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'future-canteen-2022' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'future-canteen-2022' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'future-canteen-2022' ), 'future-canteen-2022', '<a href="http://underscores.me/">James Phillips</a>' );
-				?>
+			<a href="/"><img id="footer-logo" src="/wp-content/uploads/2022/10/footer-logo.svg" /></a>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location' => 'footer-primary-menu',
+			)
+		);
+		wp_nav_menu(
+			array(
+				'theme_location' => 'footer-secondary-menu',
+			)
+		);
+		?>
+		<div id="copyright">
+			<p>Copyright <?php echo date("Y"); ?> © T&T Management Group Pty Ltd T/A Future Canteen.</p>
+			<p>ABN: 31 601 944 941</p>
+		</div>
+	</div><!-- #footer-inner -->
+	<img src='/wp-content/uploads/2022/10/footer-banner.svg' />
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
