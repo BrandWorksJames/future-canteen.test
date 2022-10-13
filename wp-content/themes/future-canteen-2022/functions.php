697c7449aea3c6f5d55d0976aaa8e613f35693a4
James Phillips <james@brandworks.co>
2022-10-13 12:20:32 +1100

    Fixed Favicon for admin screens

--- wp-content/themes/future-canteen-2022/functions.php
+++ wp-content/themes/future-canteen-2022/functions.php
@@ -184,3 +184,5 @@ function add_favicon(){
     echo "<link rel='shortcut icon' href='" . get_stylesheet_directory_uri() . "/favicon.png' />" . "\n";
 }
 add_action( 'wp_head', 'add_favicon');
+add_action('login_head', 'add_favicon');
+add_action('admin_head', 'add_favicon');
